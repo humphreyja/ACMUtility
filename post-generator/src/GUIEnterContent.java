c27984efc6bce20db0c9a1565d2f878c24b5a9ea
jake <jakehumphrey18@gmail.com>
2015-02-23 04:30:38 -0600

    creating project

---
+++ post-generator/src/GUIEnterContent.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GUIEnterContent extends JFrame{
+	private JTextField postName;
+	private JTextField postDate;
+	private JTextField postMatierial;
+	private JTextField postContent;
+	private JTextField postAuthor;
+	private JTextField postLocation;
+	private JTextField postDuration;
+	private JComboBox eventType;
+	
+	private String[] types_of_events = {"event","article","info"};
+	
+	public GUIEnterContent()
+	{
+		super("MSUM-ACM Post Generator");
+		setLayout(new FlowLayout());
+		
+		postName = new JTextField();
+		postDate = new JTextField();
+		postMatierial = new JTextField();
+		postContent = new JTextField();
+		postAuthor = new JTextField();
+		postLocation = new JTextField();
+		postDuration = new JTextField();
+		eventType = new JComboBox(types_of_events);
+		eventType.setFocusable(false);
+		
+		add(postName);
+		add(eventType);
+		add(postAuthor);
+		add(postDate);
+		add(postLocation);
+		add(postDuration);
+		add(postMatierial);
+		add(postContent);
+	}
+	
+}
